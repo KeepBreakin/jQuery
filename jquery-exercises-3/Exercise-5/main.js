8c5b539e7b17d639e3aff29b16f296774e33a0e4
Manuel Colomo <manuel.colomo89@gmail.com>
2019-03-27 16:55:49 +0100

    exercise 3 done

--- jquery-exercises-3/Exercise-5/main.js
+++ jquery-exercises-3/Exercise-5/main.js
@@ -1,12 +1,32 @@
-$(document).ready(function() {
-  let count = 0;
-  $("#btnOne").click(function() {
-    count++;
-    $("#input").val(count);
-  });
-
-  $("#btnTwo").click(function() {
-    count--;
-    $("#input").val(count);
-  });
+$(document).keydown(function(e) {
+  switch (e.which) {
+    case 37:
+      $("#square")
+        .finish()
+        .animate({
+          left: "-=10"
+        });
+      break;
+    case 38:
+      $("#square")
+        .finish()
+        .animate({
+          top: "-=10"
+        });
+      break;
+    case 39:
+      $("#square")
+        .finish()
+        .animate({
+          left: "+=10"
+        });
+      break;
+    case 40:
+      $("#square")
+        .finish()
+        .animate({
+          top: "+=10"
+        });
+      break;
+  }
 });
